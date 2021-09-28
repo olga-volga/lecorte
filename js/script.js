583c852c4b59e8305d22f45c54d1d3cb9c891822
olga <yaolmi@yandex.ru>
2021-09-28 16:12:31 +0700

    add js polyfills

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-"use strict";
-
 require('es6-promise').polyfill();
+require ('es7-object-polyfill');
+import 'core-js';
 
 import menu from './modules/menu';
 import form from './modules/form';
 
 window.addEventListener('DOMContentLoaded', function() {
-
+	"use strict";
 	menu();
 	form();
 });
